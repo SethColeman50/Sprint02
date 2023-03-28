5ffe4a7c77fa369ec3175292998fb8d2e74cfc05
Owen Halliday <drek@riseup.net>
2023-03-28 09:42:11 -0400

    renamed file

--- src/Project02/WeebWizardStrat.java
+++ src/Project02/WeebWizardStrat.java
@@ -4,13 +4,12 @@ package Project02;
  * Implementation of the WeebWizard class. A wizard so obsessed with Japanese media that they try to copy it.
  * It has the potential to deal a lot of damage, but dies very easily due to how chances there are for it to take a lot of damage too.
  */
-public class WeebWizardStart implements Strategy
+public class WeebWizardStrat implements Strategy
 {
     /**
      * The main implementation of the strategy. When encountering an opponent, it can deal massive damage other Wizards
      * as casting such a strong spell takes a lot of time, so Warriors interrupt the cast by attacking them.
-     * But casting the spell, which deals damage equal to total health atm, kills them off.
-     *
+     * But casting the spell, which deals damage equal to total health atm, kills them off.*
      * When meeting someone from the same nation, they beat up warriors after getting mad at them, get cocky and die
      * when below 50 and can cast a heal when above 50 for their current amount.
      * @param me - person to get life points for.
