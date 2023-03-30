f48d659d181a13b13d91d10574eb6e62077c8f7f
Seth Coleman <colemans@moravian.edu>
2023-03-30 10:07:47 -0400

    Add Seth's wizard strategy and tests for it

--- src/Project02/SethWizard.java
+++ src/Project02/SethWizard.java
@@ -1,5 +1,8 @@
 package Project02;
 
+/**
+ * Wizard that uses the SethWizard Strategy
+ */
 public class SethWizard extends People {
     SethWizard(String nation, String tribe, int lifePoints, Strategy warriorStrategy) {
         super(nation, tribe, PeopleType.wizard, lifePoints, warriorStrategy);
