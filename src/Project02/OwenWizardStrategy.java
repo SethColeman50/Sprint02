4393ac329c7b531d3216308162627f52602d0db5
Owen Halliday <drek@riseup.net>
2023-04-04 11:06:24 -0400

    minor changes to strat

--- src/Project02/OwenWizardStrategy.java
+++ src/Project02/OwenWizardStrategy.java
@@ -8,14 +8,13 @@ public class OwenWizardStrategy implements Strategy {
             if (otherPerson.getType() == PeopleType.wizard) {
                 return 0;
             }
-            //take however many hp is required to get back to 100, or if the otherPerson has less than that, all their hp but one
-            if (otherPerson.getLifePoints() >= 100 - me.getLifePoints()) {
-                return (100 - otherPerson.getLifePoints());
+            if (otherPerson.getLifePoints() > me.getLifePoints()) {
+                return -(100 - otherPerson.getLifePoints());
             } else {
-                return (100 - me.getLifePoints());
+                return -(100 - me.getLifePoints());
             }
         } else {
-            //if the other person is a warrior, run away
+            //if the other person is a warrior, run away, do no damage
             if (otherPerson.getType() == PeopleType.warrior) {
                 return 0;
             }
@@ -23,7 +22,8 @@ public class OwenWizardStrategy implements Strategy {
             if (otherPerson.getType() == PeopleType.wizard) {
                 return (me.getLifePoints() / 2);
             }
+            return 0;
         }
-        return 0;
+
     }
 }
