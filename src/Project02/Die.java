2ee79b3816ab88d05f42cda84d0cc189a4ccac9e
Seth Coleman <colemans@moravian.edu>
2023-04-18 10:25:31 -0400

    Refactor dies sides into roll method and added tests and documentation

--- src/Project02/Die.java
+++ src/Project02/Die.java
@@ -3,16 +3,21 @@ package Project02;
 import java.time.Instant;
 import java.util.Random;
 
+/**
+ * A singleton that controls all RNG in the program, abstracted to a die that can have an arbitrary number of sides
+ */
 public class Die {
     private static Die onlyDie;
     private final Random rng;
-    private int numSides;
     private Die() {
         // Seeds the rng with the current epoch
         this.rng = new Random(Instant.now().toEpochMilli());
-        this.numSides = 3;
     }
 
+    /**
+     * Gets the one die instance so all rng values are on the same seed
+     * @return the Die class
+     */
     public static Die getInstance() {
         if (onlyDie == null) {
             onlyDie = new Die();
@@ -22,15 +27,15 @@ public class Die {
 
     // TODO: maybe a set seed
 
-    public void setSides(int sides) {
+    /**
+     * Rolls a die with a number of sides
+     * @param sides the number of sides the die has
+     * @return the number the die rolls
+     */
+    public int roll(int sides) {
         if (sides  < 3) {
             throw new IllegalArgumentException("Sides must be greater than 2");
         }
-
-        this.numSides = sides;
-    }
-
-    public int roll() {
-        return rng.nextInt(numSides) + 1;
+        return rng.nextInt(sides) + 1;
     }
 }
