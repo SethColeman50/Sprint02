e2bfb791c99f2739656b5cd53063e3379fbce9f0
Seth Coleman <colemans@moravian.edu>
2023-04-29 12:24:55 -0400

    Added a second Strategy for warriors

--- tests/Project02/SethStrategyTest.java
+++ tests/Project02/SethStrategyTest.java
@@ -29,6 +29,30 @@ class SethStrategyTest {
     }
 
     @Test
+    public void testWarriorStrat2() {
+        Strategy strategy = new SethWarriorStrat2();
+        int maxLifePoints = 100;
+
+        People MainPerson = new testPerson("testNation", "testTribe", maxLifePoints, strategy);
+
+        // Friendly Case
+        People FriendlyPerson = new testPerson("testNation", "otherTribe", maxLifePoints, strategy);
+        assertEquals(0, MainPerson.encounterLifePoints(MainPerson, FriendlyPerson));
+
+        // Enemy Case if at full health
+        People FullHealthEnemyPerson = new testPerson("otherNation", "otherTribe", maxLifePoints, strategy);
+        assertEquals(5, MainPerson.encounterLifePoints(MainPerson, FullHealthEnemyPerson));
+
+        // Enemy Case if at 1/6 health
+        People HalfHealthEnemyPerson = new testPerson("otherNation", "otherTribe", maxLifePoints / 4, strategy);
+        assertEquals(5, MainPerson.encounterLifePoints(MainPerson, HalfHealthEnemyPerson));
+
+        // Same Tribe Case
+        People TribePerson = new testPerson("testNation", "testTribe", maxLifePoints, strategy);
+        assertEquals(5, MainPerson.encounterLifePoints(MainPerson, TribePerson));
+    }
+
+    @Test
     public void testWizardStrategy() {
         Strategy strategy = new SethWizardStrategy();
         int maxLifePoints = 100;
