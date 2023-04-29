136fb24417e39bf4740872b03e748b7147f177f8
Owen Halliday <drek@riseup.net>
2023-04-29 13:59:04 -0400

    artifacts will now die after two uses

--- src/Project02/Artifacts.java
+++ src/Project02/Artifacts.java
@@ -1,15 +1,28 @@
 package Project02;
 
+import java.util.ArrayList;
+
 public class Artifacts extends AbstractTribe
 {
     public Artifacts()
     {
         nationName = "Artifact";
         tribeName = "Artifact";
-        tribeLifePoints = 200;
+        tribeLifePoints = 1000;
 
 //        members.add();
     }
 
+    @Override
+    public ArrayList<People> getLivingTribeMembers() {
+        livingMembers.clear();
 
+        for (People person:
+             members) {
+            if (person.uses < 2) {
+                livingMembers.add(person);
+            }
+        }
+        return livingMembers;
+    }
 }
