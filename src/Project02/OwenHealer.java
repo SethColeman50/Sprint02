3be7458114fbd022ae07f3bb3349a8c5398332c1
Owen Halliday <drek@riseup.net>
2023-04-29 12:27:53 -0400

    implemented healer

---
+++ src/Project02/OwenHealer.java
@@ -0,0 +1,27 @@
+package Project02;
+
+import Project02.People;
+import Project02.PeopleType;
+import Project02.Strategy;
+
+public class OwenHealer extends People {
+    Strategy strat1;
+    Strategy strat2;
+
+    OwenHealer(String nation, String tribe, int lifePoints, Strategy strat1, Strategy strat2) {
+        super(nation, tribe, PeopleType.warrior, lifePoints, strat1);
+        this.strat1 = strat1;
+        this.strat2 = strat2;
+        myDescription = "\tOwen Healer";
+    }
+
+    @Override
+    public int encounterLifePoints(People me, People otherPerson) {
+        //if above 75 health use encounterStrategy 1;
+        if (getLifePoints() > 75)
+            encounterStrategy = this.strat1;
+        else
+            encounterStrategy = this.strat2;
+        return encounterStrategy.strategy(me, otherPerson);
+    }
+}
