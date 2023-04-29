29e736b95311c7494fd942694fd9e064ba72d0b9
Owen Halliday <drek@riseup.net>
2023-04-29 12:28:16 -0400

    changed existing strategies slightly

--- src/Project02/OwenWizardStrategy.java
+++ src/Project02/OwenWizardStrategy.java
@@ -4,22 +4,22 @@ public class OwenWizardStrategy implements Strategy {
     //wizards will run away from all battles and heal warriors from their own tribe
     public int strategy(People me, People otherPerson) {
         if (me.getNation().equals(otherPerson.getNation())) {
-            //if the other person is a wizard nothing happens
+            //if the other person is a wizard share life points
             if (otherPerson.getType() == PeopleType.wizard) {
-                return 0;
+                if (otherPerson.getLifePoints() > me.getLifePoints()) {
+                    return -(100 - otherPerson.getLifePoints());
+                } else {
+                    return -(100 - me.getLifePoints());
+                }
             }
-            if (otherPerson.getLifePoints() > me.getLifePoints()) {
-                return -(100 - otherPerson.getLifePoints());
-            } else {
-                return -(100 - me.getLifePoints());
-            }
+            return 0;
         } else {
-            //if the other person is a warrior, run away, do no damage
+            //if the other person is a warrior, do some damage
             if (otherPerson.getType() == PeopleType.warrior) {
-                return 0;
+                return (int)((100 - otherPerson.getLifePoints())/5.0);
             }
-            //if the other person is a wizard, attack
-            if (otherPerson.getType() == PeopleType.wizard) {
+            //if the other person is a wizard or healer, attack
+            if (otherPerson.getType() == PeopleType.wizard || otherPerson.getType() == PeopleType.healer)  {
                 return (me.getLifePoints() / 2);
             }
             return 0;
