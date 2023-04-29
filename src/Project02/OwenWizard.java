d2ead0e90c930c9c1c29f10c2c787575f5a422c9
Owen Halliday <drek@riseup.net>
2023-04-29 12:28:26 -0400

    updated warrior and wizard to take in both strategies

--- src/Project02/OwenWizard.java
+++ src/Project02/OwenWizard.java
@@ -2,12 +2,21 @@ package Project02;
 
 
 public class OwenWizard extends People {
-    OwenWizard(String nation, String tribe, int lifePoints, Strategy warriorStrategy) {
-        super(nation, tribe, PeopleType.wizard, lifePoints, warriorStrategy);
+    Strategy strat1;
+    Strategy strat2;
+    OwenWizard(String nation, String tribe, int lifePoints, Strategy strat1, Strategy strat2) {
+        super(nation, tribe, PeopleType.wizard, lifePoints, strat1);
+        this.strat1 = strat1;
+        this.strat2 = strat2;
         myDescription = "\tOwen Wizard";
     }
     @Override
     public int encounterLifePoints(People me, People otherPerson) {
+        //if above 75 health use encounterStrategy 1;
+        if (getLifePoints() > 75)
+            encounterStrategy = this.strat1;
+        else
+            encounterStrategy = this.strat2;
         return encounterStrategy.strategy(me, otherPerson);
     }
 
