b013d6395c3e3e18db85db1cec54b84cc7ac5e73
Owen Halliday <drek@riseup.net>
2023-04-29 13:37:01 -0400

    basic artifact class

---
+++ src/Project02/Artifacts.java
@@ -0,0 +1,15 @@
+package Project02;
+
+public class Artifacts extends AbstractTribe
+{
+    public Artifacts()
+    {
+        nationName = "Artifact";
+        tribeName = "Artifact";
+        tribeLifePoints = 200;
+
+//        members.add();
+    }
+
+
+}
