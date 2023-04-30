18ebd25b2c03bcc15b6136e1c5c441f5d0d4e7db
Owen Halliday <drek@riseup.net>
2023-04-30 18:44:56 -0400

    renamed bottomless pit

--- src/Project02/BottomlessPit.java
+++ src/Project02/BottomlessPit.java
@@ -6,7 +6,7 @@ public class BottomlessPit extends People{
     BottomlessPit(String nation, String tribe, int lifePoints)
     {
         super(nation, tribe, PeopleType.artifact, lifePoints, new NothingStrategy());
-        myDescription = "The Fountain of Youth";
+        myDescription = "Bottomless Pit";
     }
     @Override
     public int encounterLifePoints(People me, People otherPerson) {
