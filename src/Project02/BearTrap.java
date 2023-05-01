9d1210e031fef036883aae2e491b6495f896daba
Seth Coleman <colemans@moravian.edu>
2023-04-30 21:11:21 -0400

    documented bear trap

--- src/Project02/BearTrap.java
+++ src/Project02/BearTrap.java
@@ -1,11 +1,20 @@
 package Project02;
 
+/**
+ * Bear trap artifact that will hurt the player who steps on it
+ */
 public class BearTrap extends People {
     BearTrap(String nation, String tribe, int lifePoints) {
         super(nation, tribe, PeopleType.artifact, lifePoints, new NothingStrategy());
         myDescription = "Bear trap";
     }
 
+    /**
+     * 50% chance to be a small or big trap, does a lot of damage
+     * @param me, the bear trap
+     * @param otherPerson the person who stepped in
+     * @return life points, always zero because it doesn't attack
+     */
     @Override
     public int encounterLifePoints(People me, People otherPerson) {
         int roll = (Die.getInstance().roll(4) - 1) / 2;
