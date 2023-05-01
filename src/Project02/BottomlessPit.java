8b69a5dedc59827e147a646d454e7aea73951f8b
Owen Halliday <drek@riseup.net>
2023-04-30 21:21:21 -0400

    added documentation and added an artifact check to encounter

--- src/Project02/BottomlessPit.java
+++ src/Project02/BottomlessPit.java
@@ -3,15 +3,29 @@ package Project02;
 public class BottomlessPit extends People{
 
 
+    /**
+     * Bottomless pit that will kill a person who falls into it.
+     * @param nation the nation this artifact belongs to
+     * @param tribe the tribe this artifact belongs to
+     * @param lifePoints number of life points this artifact has
+     */
     BottomlessPit(String nation, String tribe, int lifePoints)
     {
         super(nation, tribe, PeopleType.artifact, lifePoints, new NothingStrategy());
         myDescription = "Bottomless Pit";
     }
+
+    /**
+     * kills whoever falls into the bottomless pit
+     * @param me, the bottomless pit
+     * @param otherPerson the person who fell in
+     * @return life points, always zero because it doesn't attack
+     */
     @Override
     public int encounterLifePoints(People me, People otherPerson) {
-        otherPerson.setDead();
+        if (otherPerson.getType() != PeopleType.artifact)
+            otherPerson.setDead();
         uses++;
-        return encounterStrategy.strategy(me,otherPerson);
+        return 0;
     }
 }
--- src/Project02/FountainOfYouth.java
+++ src/Project02/FountainOfYouth.java
@@ -2,15 +2,29 @@ package Project02;
 
 public class FountainOfYouth extends People {
 
+
+    /**
+     * the fountain of youth, heals someone to full when they find it
+     * @param nation the nation this artifact belongs to
+     * @param tribe the tribe this artifact belongs to
+     * @param lifePoints number of life points this artifact has
+     */
     FountainOfYouth(String nation, String tribe, int lifePoints)
     {
         super(nation, tribe, PeopleType.artifact, lifePoints, new NothingStrategy());
-        myDescription = "The Fountain of Youth";
+        myDescription = "Fountain of Youth";
     }
 
+    /**
+     * heals to full whoever finds the fountain of youth
+     * @param me, the fountain of youth
+     * @param otherPerson the person who found it
+     * @return life points, always zero because it doesn't attack
+     */
     @Override
     public int encounterLifePoints(People me, People otherPerson) {
-        otherPerson.modifyLifePoints((100-otherPerson.getLifePoints()));
+        if (otherPerson.getType() != PeopleType.artifact)
+            otherPerson.modifyLifePoints((100-otherPerson.getLifePoints()));
         uses++;
         return encounterStrategy.strategy(me,otherPerson);
     }
